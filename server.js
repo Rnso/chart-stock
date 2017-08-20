23bdc685dc791ea7483e2c9d2eef05b5b9af8d4a
Rnso <rnso.pnr@gmail.com>
2017-08-20 14:45:46 +0400

    initial upload to github

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import config from './config'
 import express from 'express'
 import path from 'path'
-import bodyParser from 'body-parser'
 
 
 const server = express()
 
-server.use(bodyParser.json() )
 server.use(express.static('dist'))
 
 
