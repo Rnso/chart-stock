78dc0d893ccfd002efaf0b8366ac3bc53c0186ff
Aso <rnso.pnr@gmail.com>
2017-08-31 12:02:37 +0400

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
 //export const serverUrl = 'https://nightlife-cordination.herokuapp.com'
 //export const serverUrl = 'http://localhost:9000'
 
-export const API_KEY = '8RFZLXMVZ5D9UITC'  // Alpha Vantage
+export const API_KEY = 'YOUR KEY'  // Alpha Vantage
